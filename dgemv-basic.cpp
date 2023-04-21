3efc2a7fd3e80bdd08854d6b9abd5d0d9263a575
Rutu Patel <patelrutvij2801@gmail.com>
2023-04-20 20:12:54 -0700

    Create dgemv-basic.cpp

--- dgemv-basic.cpp
+++ dgemv-basic.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+using namespace std;
+
 const char* dgemv_desc = "Basic implementation of matrix-vector multiply.";
 
 /*
@@ -7,5 +10,29 @@ const char* dgemv_desc = "Basic implementation of matrix-vector multiply.";
  * On exit, A and X maintain their input values.
  */
 void my_dgemv(int n, double* A, double* x, double* y) {
-   // insert your code here: implementation of basic matrix multiply
+    // implementation of basic matrix multiply
+    for (int i = 0; i < n; i++) {
+        double sum = 0.0;
+        for (int j = 0; j < n; j++) {
+            sum += A[i*n+j] * x[j];
+        }
+        y[i] += sum;
+    }
+}
+
+int main() {
+    int n = 3;
+    double A[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    double x[3] = {1, 2, 3};
+    double y[3] = {0, 0, 0};
+
+    my_dgemv(n, A, x, y);
+
+    // print the result vector
+    for (int i = 0; i < n; i++) {
+        cout << y[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
