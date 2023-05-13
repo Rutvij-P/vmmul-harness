eb31a6386c2321a79a2156bc23d89b133bd59505
Rutu Patel <patelrutvij2801@gmail.com>
2023-05-12 22:54:26 -0700

    configure cmakelist

--- dgemv-openmp.cpp
+++ dgemv-openmp.cpp
@@ -1,62 +1,25 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <omp.h>
 
 const char* dgemv_desc = "OpenMP dgemv.";
 
-enum DgemvMode {
-  DGEMV_OUTER,
-  DGEMV_INNER,
-  DGEMV_NESTED
-};
+/*
+ * This routine performs a dgemv operation
+ * Y :=  A * X + Y
+ * where A is n-by-n matrix stored in row-major format, and X and Y are n by 1 vectors.
+ * On exit, A and X maintain their input values.
+ */
 
-void my_dgemv(int n, double* A, double* x, double* y, DgemvMode mode) {
-  #pragma omp parallel for
-  for (int i = 0; i < n; i++) {
-    double sum = 0.0;
-    switch (mode) {
-      case DGEMV_OUTER:
-        for (int j = 0; j < n; j++) {
-          sum += A[i * n + j] * x[j];
-        }
-        break;
-      case DGEMV_INNER:
-        #pragma omp parallel for reduction(+:sum)
-        for (int j = 0; j < n; j++) {
-          sum += A[i * n + j] * x[j];
-        }
-        break;
-      case DGEMV_NESTED:
-        #pragma omp simd reduction(+:sum)
-        for (int j = 0; j < n; j++) {
-          sum += A[i * n + j] * x[j];
-        }
-        break;
-    }
-    #pragma omp atomic
-    y[i] += sum;
-  }
-}
-
-extern "C" {
-  void benchmark(int n, double* A, double* x, double* y, int mode) {
-    DgemvMode dgemvMode;
-    switch (mode) {
-      case 0:
-        dgemvMode = DGEMV_OUTER;
-        break;
-      case 1:
-        dgemvMode = DGEMV_INNER;
-        break;
-      case 2:
-        dgemvMode = DGEMV_NESTED;
-        break;
-      default:
-        fprintf(stderr, "Invalid mode\n");
-        return;
-    }
-
-    my_dgemv(n, A, x, y, dgemvMode);
-  }
+void my_dgemv(int n, double* A, double* x, double* y) {
+   #pragma omp parallel for
+   for (int i = 0; i < n; i++) {
+      double sum = 0.0;
+      for (int j = 0; j < n; j++) {
+         sum += A[i*n+j] * x[j];
+      }
+      #pragma omp atomic
+      y[i] += sum;
+   }
 }
