03f2b453799b966bba40537b3a46f59143ccd7a4
Rutu Patel <patelrutvij2801@gmail.com>
2023-05-12 20:50:59 -0700

    extra credit

---
+++ dgemv_innerloop.cpp
@@ -0,0 +1,24 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <omp.h>
+
+const char* dgemv_desc = "OpenMP dgemv.";
+
+/*
+ * This routine performs a dgemv operation
+ * Y :=  A * X + Y
+ * where A is n-by-n matrix stored in row-major format, and X and Y are n by 1 vectors.
+ * On exit, A and X maintain their input values.
+ */
+
+void my_dgemv(int n, double* A, double* x, double* y) {
+   for (int i = 0; i < n; i++) {
+      double sum = 0.0;
+      #pragma omp parallel for reduction(+:sum)
+      for (int j = 0; j < n; j++) {
+         sum += A[i*n+j] * x[j];
+      }
+      y[i] += sum;
+   }
+}
---
+++ dgemv_nestedloop.cpp
@@ -0,0 +1,26 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <omp.h>
+
+const char* dgemv_desc = "OpenMP dgemv.";
+
+/*
+ * This routine performs a dgemv operation
+ * Y :=  A * X + Y
+ * where A is n-by-n matrix stored in row-major format, and X and Y are n by 1 vectors.
+ * On exit, A and X maintain their input values.
+ */
+
+void my_dgemv(int n, double* A, double* x, double* y) {
+   #pragma omp parallel for
+   for (int i = 0; i < n; i++) {
+      double sum = 0.0;
+        #pragma omp simd reduction(+:sum)
+      for (int j = 0; j < n; j++) {
+         sum += A[i*n+j] * x[j];
+      }
+      #pragma omp atomic
+      y[i] += sum;
+   }
+}
