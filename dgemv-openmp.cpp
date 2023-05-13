d0a4123fd0a4f6af268339c19c95b68b82fcb120
Rutu Patel <patelrutvij2801@gmail.com>
2023-05-12 22:31:36 -0700

    debugging

--- dgemv-openmp.cpp
+++ dgemv-openmp.cpp
@@ -5,38 +5,42 @@
 
 const char* dgemv_desc = "OpenMP dgemv.";
 
-void my_dgemv_outer(int n, double* A, double* x, double* y) {
-   #pragma omp parallel for
-   for (int i = 0; i < n; i++) {
-      double sum = 0.0;
-      for (int j = 0; j < n; j++) {
-         sum += A[i*n+j] * x[j];
-      }
-      #pragma omp atomic
-      y[i] += sum;
-   }
-}
-
-void my_dgemv_inner(int n, double* A, double* x, double* y) {
-   for (int i = 0; i < n; i++) {
-      double sum = 0.0;
-      #pragma omp parallel for reduction(+:sum)
-      for (int j = 0; j < n; j++) {
-         sum += A[i*n+j] * x[j];
-      }
-      y[i] += sum;
-   }
-}
-
-void my_dgemv_nested(int n, double* A, double* x, double* y) {
-   #pragma omp parallel for
-   for (int i = 0; i < n; i++) {
-      double sum = 0.0;
-      #pragma omp simd reduction(+:sum)
-      for (int j = 0; j < n; j++) {
-         sum += A[i*n+j] * x[j];
-      }
-      #pragma omp atomic
-      y[i] += sum;
+void my_dgemv(int n, double* A, double* x, double* y, int mode) {
+   switch (mode) {
+      case 0: // my_dgemv_outer
+         #pragma omp parallel for
+         for (int i = 0; i < n; i++) {
+            double sum = 0.0;
+            for (int j = 0; j < n; j++) {
+               sum += A[i * n + j] * x[j];
+            }
+            #pragma omp atomic
+            y[i] += sum;
+         }
+         break;
+      case 1: // my_dgemv_inner
+         for (int i = 0; i < n; i++) {
+            double sum = 0.0;
+            #pragma omp parallel for reduction(+:sum)
+            for (int j = 0; j < n; j++) {
+               sum += A[i * n + j] * x[j];
+            }
+            y[i] += sum;
+         }
+         break;
+      case 2: // my_dgemv_nested
+         #pragma omp parallel for
+         for (int i = 0; i < n; i++) {
+            double sum = 0.0;
+            #pragma omp simd reduction(+:sum)
+            for (int j = 0; j < n; j++) {
+               sum += A[i * n + j] * x[j];
+            }
+            #pragma omp atomic
+            y[i] += sum;
+         }
+         break;
+      default:
+         printf("Invalid mode!\n");
    }
 }
